ab19255a293fad2ecaabb33fe182e642f5a70f11
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2024-10-15 16:37:30 +1100

    add lb, tg, and listener

--- main.tf
+++ main.tf
@@ -192,15 +192,48 @@ resource "aws_autoscaling_group" "web-asg" {
   desired_capacity   = 2
   max_size           = 4
   min_size           = 2
+  health_check_type = "ELB"
 
   launch_template {
     id      = aws_launch_template.web-template.id
     version = aws_launch_template.web-template.latest_version
   }
 
+  target_group_arns = [ aws_lb_target_group.web-tg.arn ]
+
   tag {
     key                 = "Name"
     value               = "Web Spot"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "web-lb" {
+  name               = "web-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web.id]
+  subnets            = [aws_subnet.public-sub-1.id, aws_subnet.public-sub-2.id]
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "web-tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my-vpc.id
+}
+
+resource "aws_lb_listener" "web-lb-listener" {
+  load_balancer_arn = aws_lb.web-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web-tg.arn
   }
 }
