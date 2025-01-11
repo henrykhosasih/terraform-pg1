7303a35458267deb831f586d1eef03d028a72262
HenryKhosasih <68894872+HenryKhosasih@users.noreply.github.com>
2025-01-11 22:07:51 +1100

    update web

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
 yum install -y httpd
-echo "Hello, Henry! This instance is running in AZ: $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)" > /var/www/html/index.html
+echo "Hi, Henry! This instance is running in AZ: $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)" > /var/www/html/index.html
 systemctl start httpd
 systemctl enable httpd
